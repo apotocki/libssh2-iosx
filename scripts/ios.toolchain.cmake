f8ce6b64ceb32a41ebef0e33f305a4a2a1f3c182
Alexander Pototskiy <alex.a.potocki@gmail.com>
2023-03-14 13:04:51 +0100

    +github actions build script

--- scripts/ios.toolchain.cmake
+++
@@ -1,9 +0,0 @@
-# this one is important
-set (BUILD_IOS True)
-
-# the name of the target operating system
-SET(CMAKE_SYSTEM_NAME iOS)
-
-#SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
-
-SET(DCMAKE_OSX_ARCHITECTURES arm64)
